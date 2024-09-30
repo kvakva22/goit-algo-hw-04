34ab478ec82b527aa2a21976877cf28aa602fbd2
kvakva22 <qwexykk@gmail.com>
2024-09-30 22:31:32 +0300

    Update homework1.py

--- homework1.py
+++ homework1.py
@@ -1,23 +1,23 @@
 from pathlib import Path
-path = 'plata.txt'
 
 
 def total_salary(path):
     total = 0
     count = 0
-
     try:
         with open(path, 'r') as fh:
             for line in fh:
                 name, salary = line.split(',')
-                total += int(salary)
+                total += float(salary)
                 count += 1
-        average = int(total / count)
-        result = f'Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}'
-    except FileNotFoundError:
-        result = print(f'Вказаного файла не існує або шлях "{path}" не є правильним')
-        
+            average = total / count
+            return total, average
+    except (FileNotFoundError, ZeroDivisionError):
+            return None
 
-    return result
-
-print(total_salary(path))
+result = total_salary('plata.txt')
+if result is not None:    
+    total, average = total_salary('plata.txt')
+    print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+else:
+     print('Файл не був знайден або він пустий')
